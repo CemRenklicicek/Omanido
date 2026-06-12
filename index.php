225877912e05ad19666abc46b825b42a39bb270f
Cem Renklicicek <85224831+CemRenklicicek@users.noreply.github.com>
2026-06-12 12:02:14 +0200

    Prevent SQL injection in login process

--- index.php
+++ index.php
@@ -12,25 +12,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // kwetsbaar voor SQL injectie
-    $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
-    $result = $pdo->query($sql);
-    $user = $result->fetch();
+$sql = "SELECT * FROM user WHERE username = :username AND password = :password";
+$stmt = $pdo->prepare($sql);
 
-    // Controleer of er een rij is gevonden
-    if($result->rowCount() > 0) {
-        // Gebruiker is ingelogd
-        $_SESSION['loggedin'] = true;
-        $_SESSION['username'] = $username;
-        $_SESSION['user'] = $user;
-        var_dump($_SESSION);
+$stmt->execute([
+    ':username' => $username,
+    ':password' => $password
+]);
 
-        header("location: dashboard.php");
-        exit;
-    } else {
-        // Gebruiker is niet ingelogd
-        $error = "Gebruikersnaam of wachtwoord is onjuist";
-    }
+$user = $stmt->fetch();
+
+  if ($user) {
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    $_SESSION['user'] = $user;
+
+    header("Location: dashboard.php");
+    exit;
+} else {
+    $error = "Gebruikersnaam of wachtwoord is onjuist";
+}
 
 }
 
@@ -67,16 +68,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         <a href="register.php" class="block text-center text-sm text-blue-600 hover:underline mt-4">Nog geen account? Registreer hier</a>
     </div>
 
-    <div class="mt-4 p-2 border border-gray-300 rounded">
-        <label class="block text-sm font-medium text-gray-700">Uitgevoerde SQL-query:</label>
-        <textarea readonly class="mt-1 block w-full border rounded-md py-2 px-3 resize-none" rows="4"><? //als $sql bestaat geef $sql, anders geef aan dat deze nog niet is ingevuld
-        if(isset($sql)) {
-            echo $sql;
-        } else {
-            echo "Log in om je SQL query te zien";
-        }
-        ?></textarea>
-    </div>
 
     
 </body>
