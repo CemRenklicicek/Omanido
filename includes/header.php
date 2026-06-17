c79747c777788c58e8dcdec1e0bc219038bdae64
Cem Renklicicek <85224831+CemRenklicicek@users.noreply.github.com>
2026-06-17 09:38:10 +0200

    Remove user ID from welcome message in header

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,6 @@
 // Controleer of de gebruiker is ingelogd
 if (isset($_SESSION['loggedin'])) {
     $username = $_SESSION['user']['username'];
-    $userId = $_SESSION['user']['id'];
 }
 ?>
 
@@ -14,7 +13,7 @@ if (isset($_SESSION['loggedin'])) {
         </a>
         <?php if (isset($_SESSION['loggedin'])): ?>
             <div class="text-right">
-                <p class="text-gray-500 text-sm">Welkom,  <a href="transacties.php?id=<?= $userId ?>" class="text-blue-600 hover:underline"><?= $username ?></a></p>
+                <p class="text-gray-500 text-sm">Welkom,  <a href="transacties.php" class="text-blue-600 hover:underline"><?= $username ?></a></p>
                
             </div>
         <?php endif; ?>
