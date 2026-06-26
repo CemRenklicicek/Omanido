014fe11689ab580b2903750da29acffe399427ac
Cem Renklicicek <85224831+CemRenklicicek@users.noreply.github.com>
2026-06-26 13:35:24 +0200

    Refactor PDO connection handling in db.php

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,15 @@
 <?php
-
-
-// PDO db connection
-$host = 'localhost';  // Dit moet overeenkomen met de servicenaam van MySQL in docker-compose.yml
-$db   = 'mydb'; // De naam van je database
-$user = 'root'; // Je MySQL-gebruikersnaam
-$pass = 'root'; // Je MySQL-wachtwoord
+$host = 'localhost';
+$db   = 'mydb';
+$user = 'root';
+$pass = 'root';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+
+
+
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -16,8 +17,7 @@ $options = [
 ];
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    return new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), (int)$e->getCode());
 }
-?>
